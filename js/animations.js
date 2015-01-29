14c5240f3c6d8e1434cddbd961d311dce5130f2a
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-01-28 19:30:08 -0700

    "added verbage to edit dynamically added content via jquery"

--- js/animations.js
+++ js/animations.js
@@ -1,38 +1,83 @@
 $(document).ready(function(){
     $('#tweet-controls').hide();
+    $('.tweet-actions').hide();
+    $('.stats').hide();
+    $('.reply').hide();
     
     
     $('.tweet-compose').on('focus', function () {
-        $(this).css('height', '80px');
+        $(this).css('height', '5em');
         
         $('#tweet-controls').css('display', 'inherit');
+        
     }); 
+    
+    $('.tweet-compose').on('blur', function () {
+        if ($(this).val().length === 0) {
+            $('#tweet-controls').hide();
+            $(this).css('height', '2.5em');
+        }
+    });
     
     $('.tweet-compose').on('keyup',function() {
         
         var charLength = $(this).val().length;
-        if (charLength < 140) {
+        
+        if (charLength <= 140) {
+            
             $('#char-count').text(140 - charLength);
+            
             if (charLength > 130) {
-                $('#char-count').css('color', 'red');
-                console.log(charLength);
-            };
-            $('#char-count').css('color', '#999');
-            $('.button').removeClass('disabled');
+                
+                $('#char-count').css('color', '#E63539');
+                
+            } else {
+                
+                $('#char-count').css('color', '#999');
+                
+            }
+            
+            $('.button').removeAttr('disabled');
+            
         }   else {
-            $('.button').addClass('disabled');
+            
+            $('.button').attr('disabled','disabled');
+            
             $('#char-count').text('0');
-            $('#char-count').css('color', '#999');
      
         }
         
     });
     
     $('#tweet-submit').on('click', function () {
-        var tweet = '<li>' + $('.tweet-compose').val() + '</li>';
+        var tweet = $('.tweet-compose').val();
         var name = $('#profile-summary p').text();
-        $('#stream').prepend('<div class="tweet"> <div class="content"> <img class="avatar" src="img/alagoon.jpg" /> <strong class="fullname">' + name + '</strong> <span class="username">@superbullock</span> <p class="tweet-text">' + tweet + '</p> <div class="tweet-actions"> <ul> <li><span class="icon action-reply"></span> Reply</li> <li><span class="icon action-retweet"></span> Retweet</li> <li><span class="icon action-favorite"></span> Favorite</li> <li><span class="icon action-more"></span> More</li> </ul> </div> <div class="stats"> <div class="retweets"> <p class="num-retweets">30</p> <p>RETWEETS</p> </div> <div class="favorites"> <p class="num-favorites">6</p> <p>FAVORITES</p> </div> <div class="users-interact"> <div> <img src="img/alagoon.jpg" /> <img src="img/vklimenko.jpg" /> </div> </div> <div class="time"> 1:04 PM - 19 Sep 13 </div> </div> <div class="reply"> <img class="avatar" src="img/alagoon.jpg" /> <textarea class="tweet-compose" placeholder="Reply to @mybff"/></textarea> </div> </div> </div>');
-        
-                                                     
+        $('#stream').prepend('<div class="tweet"> <div class="content"> <img class="avatar" src="img/alagoon.jpg" /> <strong class="fullname">' + name + '</strong> <span class="username">@superbullock</span> <p class="tweet-text">' + tweet + '</p> <div class="tweet-actions"> <ul> <li><span class="icon action-reply" data-toggle="tooltip" data-original-title="Reply"></span></li> <li><span class="icon action-retweet" data-toggle="tooltip" data-original-title="Retweet"></span></li> 	<li><span class="icon action-favorite" data-toggle="tooltip" data-original-title="Favorite"></span></li> <li><span class="icon action-more" data-toggle="tooltip" data-original-title="More..."></span> </li> </ul> </div> <div class="stats"> <div class="retweets"> <p class="num-retweets">30</p> <p>RETWEETS</p> </div> <div class="favorites"> <p class="num-favorites">6</p> <p>FAVORITES</p> </div> <div class="users-interact"> <div> <img src="img/alagoon.jpg" /> <img src="img/vklimenko.jpg" /> </div> </div> <div class="time"> 1:04 PM - 19 Sep 13 </div> </div> <div class="reply"> <img class="avatar" src="img/alagoon.jpg" /> <textarea class="tweet-compose" placeholder="Reply to @mybff"/></textarea> </div> </div> </div>');
+        $('.tweet-compose').val('');
+        $('#tweet-controls').hide();
+        $('.tweet-compose').css('height', '2.5em');
+        $('#char-count').text(140);
     });
+    
+    $('#stream').on('mouseenter', '.tweet', function() {
+        $(this).find('.tweet-actions').show();
+    });
+    
+    $('#stream').on('mouseleave', '.tweet', function() {
+        $(this).find('.tweet-actions').hide();
+    });
+    
+    $('#stream').on('click','.tweet', function() {
+        $(this).find('.tweet-actions').show();
+        $(this).find('.stats').show();
+        $(this).find('.reply').show();
+    });
+    
+    $('#stream').on('dblclick', '.tweet', function() {
+        $(this).find('.tweet-actions').hide();
+        $(this).find('.stats').hide();
+        $(this).find('.reply').hide();
+    });
+    
+    $(function () { $("[data-toggle='tooltip']").tooltip(); });
 });
